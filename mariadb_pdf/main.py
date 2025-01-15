5df62a0d3ff546409959278de222f8396f0a8182
Faustin Lammler <faustin@fala.red>
2025-01-15 14:30:29 +0100

    Improve logging

--- mariadb_pdf/main.py
+++ mariadb_pdf/main.py
@@ -1,10 +1,10 @@
+from pathlib import Path
+
+from pdf.generate_pdf import generate_full_pdf
 from setup.config import read_config
 from setup.kb_urls import read_csv
 from setup.languages import read_languages
-from setup.logger import log
 
-from pdf.generate_pdf import generate_full_pdf
-from pathlib import Path
 
 def main():
     config = read_config()
@@ -12,11 +12,11 @@ def main():
     language_csvs = read_languages(csv, config)
     try:
         for lang, lang_csv in language_csvs.items():
-            log.info(f"Generating {lang}({len(lang_csv)})")
             generate_full_pdf(lang_csv, Path(f"output_{lang}"), config)
     finally:
         Path(config.wkhtml_settings["dump-outline"]).unlink(missing_ok=True)
-    log.info("Finished")
+
 
 if __name__ == "__main__":
     main()
+
--- mariadb_pdf/setup/config.py
+++ mariadb_pdf/setup/config.py
@@ -1,24 +1,26 @@
-from .logger import log
-
+import argparse
+import logging
 from dataclasses import dataclass
-from typing import Any, NamedTuple
 from pathlib import Path
+from typing import Any, NamedTuple
 
 import toml
-import argparse
 
 DEFAULT_HTML_PATH = "output.html"
 DEFAULT_PDF_PATH = "MariaDBServerKnowledgeBase.pdf"
 DEFAULT_VERBOSITY = 1
+
 
 class TocTypeConfig(NamedTuple):
     font_size: str
     padding_left: str
     margin: str
 
+
 class TocConfig(NamedTuple):
     chapter: TocTypeConfig
     main: TocTypeConfig
+
 
 # Public
 class Config(NamedTuple):
@@ -33,6 +35,7 @@ class Config(NamedTuple):
     wkhtml_settings: dict[str, Any]
     toc_config: TocConfig
 
+
 def read_config() -> Config:
     """Returns a simplified data structure containing the config settings"""
     arg_config = _read_args()
@@ -40,6 +43,7 @@ def read_config() -> Config:
     config: Config = generate_config(arg_config, dict_config)
 
     return config
+
 
 @dataclass
 class _ArgConfig:
@@ -53,6 +57,7 @@ class _ArgConfig:
     numrows: int
     quiet: bool
 
+
 def generate_config(arg_config: _ArgConfig, dict_config: dict[str, Any]) -> Config:
     return Config(
         port=arg_config.port,
@@ -61,13 +66,16 @@ def generate_config(arg_config: _ArgConfig, dict_config: dict[str, Any]) -> Conf
         repeat_outline=not arg_config.norepeat,
         languages=["en"] if not arg_config.langs else arg_config.langs,
         num_rows=-1 if arg_config.numrows is None else arg_config.numrows,
-        html_path=Path(DEFAULT_HTML_PATH if arg_config.htmlpath is None else arg_config.htmlpath),
-        pdf_path=Path(DEFAULT_PDF_PATH if arg_config.pdfpath is None else arg_config.pdfpath),
-
+        html_path=Path(
+            DEFAULT_HTML_PATH if arg_config.htmlpath is None else arg_config.htmlpath
+        ),
+        pdf_path=Path(
+            DEFAULT_PDF_PATH if arg_config.pdfpath is None else arg_config.pdfpath
+        ),
         toc_config=read_toc_config(dict_config["TOC"]),
         wkhtml_settings=dict_config["wkhtmltopdf"],
-
     )
+
 
 def _read_args() -> _ArgConfig:
     """Parses and return the information from system arguments"""
@@ -75,28 +83,69 @@ def _read_args() -> _ArgConfig:
 
     # verbosity
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-q", "--quiet", action="store_true", help="Quiet/Hide Progress Bar")
-    
-    parser.add_argument("-c", "--config", type=Path, default=Path("./config.toml"), help="config filepath")
-    parser.add_argument("-p", "--port", type=int, default=7032, help="mariadb_kb_server port")
-    parser.add_argument("-l", "--langs", type=str, nargs="+", help="Optional Languages eg: (en, it)")
-    parser.add_argument("-n", "--numrows", "--num_rows", type=int, help="Maximum Number of csv urls to use.")
-    parser.add_argument("--norepeat", action="store_true", help="Turns off repeat generation")
+    group.add_argument(
+        "-q", "--quiet", action="store_true", help="Quiet/Hide Progress Bar"
+    )
+
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=Path,
+        default=Path("./config.toml"),
+        help="config filepath",
+    )
+    parser.add_argument(
+        "-p", "--port", type=int, default=7032, help="mariadb_kb_server port"
+    )
+    parser.add_argument(
+        "-l", "--langs", type=str, nargs="+", help="Optional Languages eg: (en, it)"
+    )
+    parser.add_argument(
+        "-n",
+        "--numrows",
+        "--num_rows",
+        type=int,
+        help="Maximum Number of csv urls to use.",
+    )
+    parser.add_argument(
+        "--norepeat", action="store_true", help="Turns off repeat generation"
+    )
     parser.add_argument("--nopdf", action="store_true", help="Turns off pdf generation")
     parser.add_argument("-o", "--pdfpath", type=str, help="Path to write Final PDF")
-    parser.add_argument("--htmlpath", "--html_path", type=str, help="Path to write HTML Output")
+    parser.add_argument(
+        "--htmlpath", "--html_path", type=str, help="Path to write HTML Output"
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        help="Print lots of debugging statements",
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG,
+        default=logging.WARNING,
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Be verbose",
+        action="store_const",
+        dest="loglevel",
+        const=logging.INFO,
+    )
+    args = parser.parse_args()
+    logging.basicConfig(level=args.loglevel)
+    return parser.parse_args(namespace=_ArgConfig)  # type: ignore
 
-    return parser.parse_args(namespace=_ArgConfig) # type: ignore
 
 def read_toc_config(config: dict[str, Any]) -> TocConfig:
     main = TocTypeConfig(
         font_size=config["main_font_size"],
         padding_left=config["main_indent"],
-        margin=config["main_margin"]
+        margin=config["main_margin"],
     )
     chapter = TocTypeConfig(
         font_size=config["chapter_font_size"],
         padding_left=config["chapter_indent"],
-        margin=config["chapter_margin"]
+        margin=config["chapter_margin"],
     )
     return TocConfig(main=main, chapter=chapter)
